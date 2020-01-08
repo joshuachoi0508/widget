696ac051334316f68ad786e715043fb8c538de38
Josh Choi <joshuachoi0508@gmail.com>
2020-01-07 20:59:53 -0800

    Create weather table

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -17,7 +17,7 @@ class Weather extends React.Component {
   getLatLong(e){
     e.preventDefault();
 
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.location}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)
       .then(response => {
         return response.json();
       })
@@ -25,7 +25,11 @@ class Weather extends React.Component {
         const lat = data.results[0].geometry.location.lat;
         const lng = data.results[0].geometry.location.lng;
 
-        this.setState({ lat, lng }, this.getWeather)
+        this.setState({ 
+          lat, 
+          lng,
+          weatherLocation: data.results[0].formatted_address
+        }, this.getWeather)
       })
   }
 
@@ -33,11 +37,7 @@ class Weather extends React.Component {
     fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${process.env.REACT_APP_WEATHER_API_KEY}/${this.state.lat},${this.state.lng}`)
       .then(response => response.json())
       .then(data => {
-        debugger;
-        this.setState({ 
-          weather: data,
-          weatherLocation: this.state.location
-        })
+        this.setState({ weather: data })
       })
   }
 
@@ -50,8 +50,8 @@ class Weather extends React.Component {
           className="Weather-location-input-div"
           onSubmit={e => this.getLatLong(e)}
         >
-          <p>Enter Location</p>
-          <input 
+          <p>Enter Address</p>
+          <textarea 
             onChange={(e) => this.setState({location: e.currentTarget.value})} 
             value={this.state.location}
           />
@@ -61,20 +61,16 @@ class Weather extends React.Component {
         <div className="Weather-weather-detail">
           {this.state.weather.currently ? 
             <div>
-              <h3>{this.state.weatherLocation}'s Weather Information</h3> 
+              <h3>{this.state.weatherLocation}</h3> 
 
               <div className="Weather-current-weather-div">
                 <h4>Current Weather</h4>
-                <p>{this.state.weather.currently.summary} {this.state.weather.currently.apparentTemperature}°F</p>
+                <p>{this.state.weather.currently.summary} {Math.round(this.state.weather.currently.apparentTemperature)}°F</p>
               </div>
 
-              <div className="Weather-future-weather-div">
-                <h4>Future Weather</h4>
-                <button onClick={() => this.setState({ frame: "minutely" })}>minutely</button>
-                <button onClick={() => this.setState({ frame: "hourly" })}>Hourly</button>
-                <button onClick={() => this.setState({ frame: "daily" })}>Daily</button>
-              </div>
-
+              <WeatherTable 
+                weatherInfo={this.state}
+              />
 
             </div>
               :
--- src/components/weather_table.jsx
+++ src/components/weather_table.jsx
@@ -3,7 +3,101 @@ import React from 'react';
 class WeatherTable extends React.Component {
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      frame: "",
+      dataIdx: 0
+    };
+
+    this.decrementIdx = this.decrementIdx.bind(this);
+    this.incrementIdx = this.incrementIdx.bind(this);
+  }
+
+  decrementIdx(){
+    let { dataIdx } = this.state;
+
+    dataIdx -= 6;
+    if (dataIdx <= 0) dataIdx = 0;
+    this.setState({dataIdx});
+  }
+
+  incrementIdx() {
+    let { dataIdx } = this.state;
+    const maxIdx = this.props.weatherInfo.weather[this.state.frame].data.length - 1;
+
+    dataIdx += 6;
+    if (dataIdx >= maxIdx) dataIdx = dataIdx -= 6;
+    this.setState({ dataIdx });
+  }
+
+  createTable(){
+    const { dataIdx } = this.state;
+    const displayData = this.props.weatherInfo.weather[this.state.frame].data.slice(dataIdx, dataIdx + 6);
+
+    if (this.state.frame === "hourly") {
+      return displayData.map(weatherData => (
+        <div 
+          key={weatherData.time}
+          className="Weather-Table-individual-data"
+        >
+          <p>{new Date( weatherData.time * 1000).toGMTString().slice(5, 22)}</p>
+          <p>{Math.round(Number(weatherData.apparentTemperature))}°F</p>
+        </div>
+      ))
+    }
+
+    if (this.state.frame === "daily") {
+      return displayData.map(weatherData => (
+        <div
+          key={weatherData.time}
+          className="Weather-Table-individual-data"
+        >
+          <p>{new Date(weatherData.time * 1000).toGMTString().slice(5, 16)}</p>
+          {/* Get the average of high/low temp of the day */}
+          <p>{Math.round((Number(weatherData.apparentTemperatureHigh) + Number(weatherData.apparentTemperatureLow)) / 2)}°F</p>
+        </div>
+      ))
+    }
+  }
+
+  render(){
+    if (!this.state.frame) {
+      return (
+        <div className="Weather-future-weather-div">
+          <h4>Future Weather</h4>
+          <button onClick={() => this.setState({ frame: "hourly" })}>Hourly</button>
+          <button onClick={() => this.setState({ frame: "daily" })}>Daily</button>
+        </div>
+      )
+    }
+
+    return(
+      <div id="Weather-Table">
+        <div className="Weather-future-weather-div">
+          <h4>Future Weather</h4>
+          <button onClick={() => this.setState({ frame: "hourly", dataIdx: 0 })}>Hourly</button>
+          <button onClick={() => this.setState({ frame: "daily", dataIdx: 0 })}>Daily</button>
+        </div>
+
+        <h3>Showing {this.state.frame} Weather Info</h3>
+
+        <div className="Weather-Table-tables-div">
+          {this.createTable()}
+
+          <div className="Weather-table-arrow-div">
+            <p 
+              className="Weather-table-arrow"
+              onClick={this.decrementIdx}
+            >⇦</p> 
+
+            <p 
+              className="Weather-table-arrow"
+              onClick={this.incrementIdx}
+            >⇨</p>
+          </div>
+
+        </div>
+      </div>
+    )
   }
 }
 
